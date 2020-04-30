47066b6dca22aed5468bfd22b6ff07f0c9ffbc6e
Sherman Chen <uxdailygeek@gmail.com>
2020-04-30 23:32:08 +0800

    Finished Code Examples for introduction

--- Microsoft_Docs/Introduction/ReferenceEquality02/Program.cs
+++ Microsoft_Docs/Introduction/ReferenceEquality02/Program.cs
@@ -1,9 +1,97 @@
-﻿namespace ReferenceEquality02
+﻿using System;
+using System.Text;
+
+namespace ReferenceEquality02
 {
-	internal class Program
+	struct TestStruct
+	{
+		public int Num { get; private set; }
+		public string Name { get; private set; }
+
+		public TestStruct ( int i, string s ) : this ()
+		{
+			Num = i;
+			Name = s;
+		}
+	}
+
+	class TestClass
+	{
+		public int Num { get; set; }
+		public string Name { get; set; }
+	}
+
+	class Program
 	{
 		public static void Main ( string [] args )
 		{
+			// Demonstrate reference equality with reference types.
+
+			#region ReferenceTypes
+
+			// create two reference type instances that have identical values.
+			TestClass tcA = new TestClass () { Num = 1, Name = "New TestClass" };
+			TestClass tcB = new TestClass () { Num = 1, Name = "New TestClass" };
+
+			Console.WriteLine ( "ReferenceEquals(tcA, tcB) = {0}", Object.ReferenceEquals ( tcA, tcB ) );
+
+			// After assignment, tcB and tcA refer to the same object/
+			// They now have reference equality.
+			tcB = tcA;
+			Console.WriteLine ( "After assignment: ReferenceEquals(tcA, tcB) = {0}",
+				Object.ReferenceEquals ( tcA, tcB ) );
+			
+			// Changes made to tcA are reflected in tcB. Therefore, objects that have reference
+			// equality also have value equality.
+			tcA.Num = 42;
+			tcA.Name = "TestClass 42";
+			Console.WriteLine ("tcB.Name = {0} tcB.Num: {1}", tcB.Name, tcB.Num);
+
+			#endregion
+			
+			// Demonstrate the two value type instances never have reference equality.
+			#region ValueTypes
+
+			TestStruct tsC = new TestStruct ( 1, "TestStruct 1" );
+			
+			// Value types are copied on assignment. tsD and tsC have
+			// the same values but are not the same object.
+			TestStruct tsD = tsC;
+			Console.WriteLine ("After assignment: ReferenceEquals(tsC, tsD) = {0}", 
+				Object.ReferenceEquals ( tsC, tsD ));
+
+			#endregion
+			
+			#region stringRefEquality
+			
+			// Constant strings within the same assembly are always interned by the runtime.
+			// This means they are stored in the same location in memory. There,
+			// the two strings have reference equality although no assignment takes place.
+			string strA = "Hello world!";
+			string strB = "Hello world!";
+			Console.WriteLine ( "ReferenceEquality(strA, strB) = {0}", Object.ReferenceEquals ( strA, strB ) );
+			
+			// After a new string is assigned to strA, strA and strB are no longer
+			// interned and no longer have reference equality.
+			strA = "Goodbye world!";
+			Console.WriteLine ( "strA = \"{0}\" strB = \"{1}\"", strA, strB );
+			Console.WriteLine ( "After strA changes, ReferenceEquals(strA, strB) = {0}",
+				Object.ReferenceEquals ( strA, strB ) );
+			
+			// A string is created at runtime cannot be interned.
+			StringBuilder sb = new StringBuilder("Hello world!");
+			string stringC = sb.ToString ();
+			Console.WriteLine ("ReferenceEquals(stringC, strB) = {0}", 
+				Object.ReferenceEquals ( stringC, strB ));
+			
+			// The string class overloads the == operator to perform an equality comparison.
+			Console.WriteLine ("stringC == strB = {0}", stringC == strB);
+
+			#endregion
+			
+			// Keep the console open in debug mode.
+			Console.WriteLine ("Press any key to exit.");
+			Console.ReadKey ();
 		}
 	}
 }
--- Microsoft_Docs/Introduction/StructValueEquality/Program.cs
+++ Microsoft_Docs/Introduction/StructValueEquality/Program.cs
@@ -1,9 +1,88 @@
-﻿namespace StructValueEquality
+﻿using System;
+using System.Collections;
+
+namespace StructValueEquality
 {
-	internal class Program
+	struct TwoDPoint : IEquatable <TwoDPoint>
+	{
+		// Read/write auto-implemented properties
+		public int X { get; private set; }
+		public int Y { get; private set; }
+
+		public TwoDPoint ( int x, int y ) : this()
+		{
+			X = x;
+			Y = y;
+		}
+
+		public override bool Equals ( object obj )
+		{
+			if ( obj is TwoDPoint )
+			{
+				return this.Equals ( ( TwoDPoint ) obj );
+			}
+
+			return false;
+		}
+
+		public bool Equals ( TwoDPoint p )
+		{
+			return ( X == p.X ) && ( Y == p.Y );
+		}
+
+		public override int GetHashCode()
+		{
+			return X ^ Y;
+		}
+
+		public static bool operator == ( TwoDPoint lhs, TwoDPoint rhs )
+		{
+			return lhs.Equals ( rhs );
+		}
+
+		public static bool operator != ( TwoDPoint lhs, TwoDPoint rhs )
+		{
+			return !( lhs.Equals ( rhs ) );
+		}
+	}
+	
+	class Program
 	{
 		public static void Main ( string [] args )
 		{
+			TwoDPoint pointA = new TwoDPoint(3, 4);
+			TwoDPoint pointB = new TwoDPoint(3, 4);
+			int i = 5;
+
+			Console.WriteLine ( "pointA.Equals(pointB) = {0}", pointA.Equals ( pointB ) );
+			Console.WriteLine ( "pointA == pointB = {0}", pointA == pointB );
+			Console.WriteLine ( "Object.Equals(pointA, pointB) = {0}", Object.Equals ( pointA, pointB ) );
+			Console.WriteLine ( "pointA.Equals(null) = {0}", pointA.Equals(null) );
+			Console.WriteLine ( "(pointA ==  null) = {0}", pointA == null );
+			Console.WriteLine ( "(pointA != null) = {0}", pointA != null );
+			Console.WriteLine ( "pointA.Equals(i) = {0}", pointA.Equals ( i ) );
+
+			// compare unboxed to boxed.
+			ArrayList list = new ArrayList ();
+			list.Add ( new TwoDPoint ( 3, 4 ) );
+			Console.WriteLine ( "pointE.Equals(list[0]): {0}", pointA.Equals ( list [ 0 ] ) );
+			
+			// Compare nullable to nullable and to non-nullable
+			TwoDPoint? pointC = null;
+			TwoDPoint? pointD = null;
+			Console.WriteLine ( "pointA == (pointC = null) = {0}", pointA ==  pointC );
+			Console.WriteLine ( "pointC == pointD = {0}", pointC == pointD );
+
+			TwoDPoint temp = new TwoDPoint ( 3, 4 );
+			pointC = temp;
+			Console.WriteLine ("pointA == (pointC = 3, 4) = {0}", pointA == pointC);
+
+			pointD = temp;
+			Console.WriteLine ("pointD == (pointC = 3, 4) = {0}", pointD == pointC);
+			
+			// Keep the console window open in debug mode.
+			Console.WriteLine ("Press any key to exit.");
+			Console.ReadKey ();
 		}
 	}
 }
