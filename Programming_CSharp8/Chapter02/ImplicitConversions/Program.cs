9eca80e63c70dcb0ce8bcdb37c3e2a0a60640955
Sherman Chen <uxdailygeek@gmail.com>
2020-05-25 21:37:13 +0800

    Added code examples for different book

---
+++ Programming_CSharp8/Chapter02/ImplicitConversions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImplicitConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
