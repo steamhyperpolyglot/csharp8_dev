839c8be219df4bfd2e6842e55b51b1b4864522ad
Sherman Chen <uxdailygeek@gmail.com>
2020-05-25 23:56:26 +0800

    Added code examples from Microsoft Docs.

---
+++ Microsoft_Docs/LocalFunctions/ExGetMultiAsync/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExGetMultiAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = GetMultipleAsync(6).Result;
+            Console.WriteLine($"The returned value is {result:N0}");
+        }
+
+        static async Task<int> GetMultipleAsync(int secondsDelay) {
+            Console.WriteLine("Executing GetMultipleAsync...");
+            if (secondsDelay < 0 || secondsDelay > 5)
+                throw new ArgumentOutOfRangeException("secondsDelay cannot exceed 5.");
+
+            await Task.Delay(secondsDelay * 1000);
+            return secondsDelay * new Random().Next(2, 10);
+        }
+    }
+}
---
+++ Microsoft_Docs/OOP/ReturnValues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReturnValues
+{
+	class Program
+	{
+		static void Main ( string [] args )
+		{
+			Console.WriteLine ( "Hello World!" );
+		}
+	}
+}
